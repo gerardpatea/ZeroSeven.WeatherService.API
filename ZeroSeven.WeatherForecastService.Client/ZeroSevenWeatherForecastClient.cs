f4f6faad65d29f8c22d6656c30ae67a4f0ae8ca0
Gerard Patea <gerard.patea@retailakumyn.com.au>
2025-06-03 09:47:22 +1000

    wip: refactoring

---
+++ ZeroSeven.WeatherForecastService.Client/ZeroSevenWeatherForecastClient.cs
@@ -0,0 +1,37 @@
+﻿
+using RestSharp;
+
+namespace ZeroSeven.WeatherForecastService.Client
+{
+    public class ZeroSevenWeatherForecastClient : IWeatherForecastService
+    {
+        #region Members
+
+        private readonly IRestClient _restClient;
+
+        #endregion
+
+        #region Constructor
+
+        public ZeroSevenWeatherForecastClient(IRestClient restSharp)
+        {
+            _restClient = restSharp;
+        }
+
+        #endregion
+
+        #region IWeatherForecastService
+
+        public async Task<GetWeatherForecastResponse> GetWeatherForecast()
+        {
+            var request = new RestRequest("/weatherforecast");
+
+            var result = await _restClient.ExecuteGetAsync<GetWeatherForecastResponse>(request);
+
+            return result.Data;
+        }
+
+        #endregion
+
+    }
+}
